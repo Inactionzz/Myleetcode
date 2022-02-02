31247ad1f76a85d258f752d5f0dee34931c4de28
Inactionzz <1173837806@qq.com>
2022-02-02 23:58:45 +0800

    2_1

---
+++ leetcode541/src/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public String reverseStr(String s, int k) {
+        StringBuffer res = new StringBuffer();
+        int length = s.length();
+        int start = 0;
+        while (start < length) {
+            // 找到k处和2k处
+            StringBuffer temp = new StringBuffer();
+            // 与length进行判断，如果大于length了，那就将其置为length
+            int firstK = (start + k > length) ? length : start + k;
+            int secondK = (start + (2 * k) > length) ? length : start + (2 * k);
+
+            //无论start所处位置，至少会反转一次
+            temp.append(s.substring(start, firstK));
+            res.append(temp.reverse());
+
+            // 如果firstK到secondK之间有元素，这些元素直接放入res里即可。
+            if (firstK < secondK) { //此时剩余长度一定大于k。
+                res.append(s.substring(firstK, secondK));
+            }
+            start += (2 * k);
+        }
+        return res.toString();
+    }
+}
