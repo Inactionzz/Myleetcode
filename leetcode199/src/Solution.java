72d7321a5e3ad5c21bc3850c9cd3de879badd18e
Inactionzz <1173837806@qq.com>
2022-02-27 22:58:19 +0800

    2_27

---
+++ leetcode199/src/Solution.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        //层次遍历，取每层最后一个元素加入到集合中
+        List<Integer> ans = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = queue.poll();
+                if (i==len-1){
+                    ans.add(node.val);
+                }
+                if (node.left!=null){
+                    queue.offer(node.left);
+                }
+                if (node.right!=null){
+                    queue.offer(node.right);
+                }
+
+            }
+        }
+        return ans;
+    }
+}
